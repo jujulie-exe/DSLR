57e03c942e3133856e85ff7aecc1ed970b4df9b6
swl <julie.exe.franco@gmail.com>
2026-02-07 19:16:46 +0100

    ADD: accuracy cheack and graf work in progress

--- logreg_train.py
+++ logreg_train.py
@@ -80,18 +80,26 @@ def calculate_step(d: npt.NDArray[np.float64], rate: float ) -> npt.NDArray[np.f
 def cheack_value_step(s: npt.NDArray[np.float64]) -> np.bool_:
     return np.all(s <= LIMIT_STEP_RATE)
 
-def gradient_descend(data: pd.DataFrame, y: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+def trace_history(history : dict, loss_cost: np.float64):
+    history["loss cost"].append(loss_cost)
+    #mi sa che mi serve solo il loss cost e salvare l'ultimo theta
+
+def gradient_descend(data: pd.DataFrame, y: npt.NDArray[np.float64]) -> tuple[npt.NDArray[np.float64], dict]:
     theta: npt.NDArray[np.float64]= np.zeros(data.shape[1])
+    history = {"theta": [], "loss cost": [], "p": []}
     for _ in range(ITERATION_RANGE):
         #z : npt.NDArray[np.float64] = theta @ data
         z = data.to_numpy() @ theta
         p = sigm(z)
+        #TODO aggiungere il loss cost corretto
+        loss_cost = np.sum(p - y)
         derivate = derivate_loss_fun(p, y, data) # manca la derivata quella non moltiplicate per le materie
         step = calculate_step(derivate, LEARNING_RATE)
+        trace_history(history, loss_cost)
         if (cheack_value_step(step)):
             break
         theta = theta - step
-    return theta
+    return theta, history
 
 def cleaning(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series , list[str]]:
     new_data = data.select_dtypes(include=[np.number]).copy()
@@ -113,6 +121,7 @@ def cleaning(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series , list[str]]:
     print(house)
     return new_data, house, materie
 
+
 def std_all_input_value(data: pd.DataFrame) -> pd.DataFrame:
     new_data = pd.DataFrame()
     for i in range(0, data.shape[1]):
@@ -123,14 +132,22 @@ def std_all_input_value(data: pd.DataFrame) -> pd.DataFrame:
     return new_data
 
 def for_loop_gradiend_descent(houses: pd.Series , data_std: pd.DataFrame, materie: list[str]) -> None:
-    data_history = []
     unique_house = houses.unique().tolist()
+    data_history: dict[str, dict[str, float]] = {}
+
     #for indice, nome in enumerate(nomi):
     for index, house in enumerate(unique_house): 
         weight: npt.NDArray[np.float64] = np.where(houses == house, 1, 0)
-        theta:  npt.NDArray[np.float64] = gradient_descend(data_std, weight)
+        theta, history = gradient_descend(data_std, weight)
+        data_history[house] = history
         print(len(materie) == len(theta))
         write_in_file(np.array(theta), house, index, PATH_OUTPUT_FILE, materie)
+    for index, house in enumerate(unique_house):
+        weight: npt.NDArray[np.float64] = np.where(houses == house, 1, 0)
+        confronti = np.where( data_history[house]['p'] > #tutti gli altri p dei altri house, 1, 0))
+                             )
+        esatti, sbagliati = count_confronti(weight, confronti)
+
 
 def main() -> None:
     data_raw: pd.DataFrame = charge_file(PATH_DATA_SET)
